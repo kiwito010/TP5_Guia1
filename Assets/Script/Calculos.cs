55b7cb852ef7284e6ab482309ee8bac49096afdc
kiwito010 <justorodrip@gmail.com>
2026-04-17 14:50:45 -0300

    Initial commit

---
+++ Assets/Script/Calculos.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calculos : MonoBehaviour
+{
+    public float num1;
+    public float num2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float resultado = num1 + num2;
+        Debug.Log(resultado);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/DatosDelUsuario.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatosDelUsuario : MonoBehaviour
+{
+        public int edad;
+        public string nombre;
+        public bool donante;
+        public float estatura;
+        string respuesta;
+    // Start is called before the first frame update
+    void Start(){
+     if (donante=true){
+        respuesta = "soy ";
+    }
+     else{
+     respuesta="no soy ";
+    }
+
+     Debug.Log("mi nombre es "+nombre +", tengo "+ edad+ " años, mido "+ estatura+" metros y "+ respuesta+ "donante.");
+    
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
