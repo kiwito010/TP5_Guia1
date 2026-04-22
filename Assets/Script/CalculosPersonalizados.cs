a8aa28b854ff361d49f5365b1676d20b41c53397
kiwito010 <justorodrip@gmail.com>
2026-04-22 09:05:32 -0300

    listo

---
+++ Assets/Script/CalculosPersonalizados.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalculosPersonalizados : MonoBehaviour
+{
+    public float num1;
+    public float num2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float[] Resultado = {num1 + num2, num1 - num2, num1 * num2, num1 / num2};
+
+        Debug.Log("La suma da "+Resultado[0]+", la resta da "+ Resultado[1]+", la multiplicacion da "+Resultado[2]+
+        " y la division da "+Resultado[3]+".");  
+    }
+}
--- Assets/Script/DeclaracionDeVariables.cs
+++ Assets/Script/DeclaracionDeVariables.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class DeclaracionDeVariables : MonoBehaviour
 {
     public int VariableInt = 923;
-    public string VariableString = maestro;
+    public string VariableString = "maestro";
     public bool VariableBool = false;
-    public float VariableFloat = 2,71592;
+    public float VariableFloat = 2.71592f;
         
     // Start is called before the first frame update
     void Start(){
-    Debug.Log(VariableBool +", "+ VariableFloat + ", "+ VariableInt+ ", "+ VariableString+".")
+    Debug.Log(VariableBool +", "+ VariableFloat + ", "+ VariableInt+ ", "+ VariableString+".");
     }
 
     // Update is called once per frame
